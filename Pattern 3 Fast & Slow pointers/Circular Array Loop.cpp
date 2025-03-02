5f914da3b71c61d238d05d3f32a6ca8163fe0df1
yashthakur16 <tyash6843@gmail.com>
2025-03-02 17:37:29 +0530

    cricular array loop solution improved

--- Pattern 3 Fast & Slow pointers/Circular Array Loop.cpp
+++ Pattern 3 Fast & Slow pointers/Circular Array Loop.cpp
@@ -3,31 +3,42 @@ class Solution {
         bool circularArrayLoop(vector<int>& nums) 
         {
             int n=nums.size();
-            
+    
             for(int i=0;i<n;i++)
             {
-             
+    
+                if(nums[i]==0) continue;                
                 int slow=i;
                 int fast=i;
+    
                 bool isPositive=nums[slow]>0;
+    
     
                 while(true)
                 {
-                    slow=nextIndex(slow,nums);
-                    if(slow==-1||(nums[slow] > 0) != isPositive) break;
+                    slow=solve(slow,isPositive,nums);
     
-                    fast=nextIndex(fast,nums);
-                    if(fast==-1||(nums[fast] > 0) != isPositive) break;
+                    if(slow==-1) break;
     
-                    fast=nextIndex(fast,nums);
-                    if(fast==-1||(nums[fast] > 0) != isPositive) break;
-                    
-                    if(slow==fast) return true;
+                    fast=solve(fast,isPositive,nums);
+    
+                    if(fast==-1) break;
+    
+                    fast=solve(fast,isPositive,nums);
+    
+                    if(fast==-1) break;
+    
+                    if(slow==fast)  return true;
                 }
     
-                
+                int index=i;
     
-               
+         
+                while (solve(index, nums[i] > 0, nums) != -1) {
+                    int temp = index;
+                    index = solve(index, nums[i] > 0, nums);
+                    nums[temp] = 0;  
+                }
             }
     
             return false;
@@ -35,11 +46,15 @@ class Solution {
         }
     
     
-        int nextIndex(int i,vector<int> & nums)
+        int solve(int i,bool isPositive,vector<int> &nums)
         {
             int n=nums.size();
-            int digit=((i + nums[i]) % n + n) % n;
-            
-            return digit==i ? -1 : digit;
+            int index=((i + nums[i]) % n + n) % n;
+    
+            if((nums[index]>0)!=isPositive) return -1;
+    
+            if(index==i) return -1;
+    
+            return index;
         }
     };
